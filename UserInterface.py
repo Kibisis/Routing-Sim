24e59df12446b77d41525ceff9e93e10eb6bb1b1
Jake French <jakefrench@Jakes-MacBook-Air.local>
2019-04-17 15:10:43 -0400

    temp

--- UserInterface.py
+++ UserInterface.py
@@ -46,7 +46,10 @@ class Router():
 
 #create routers
 def draw_routers(window, neighbor_array):
-    size = 100
+    #draw_routers(window, neighbor_array)
+    # 800 x 8000
+    size = 50
+
     r0 = Router(150, 150, size, "r0", 0)
     r1 = Router(50, 300, size, "r1", 1)
     r2 = Router(150, 450, size, "r2", 2)
@@ -103,6 +106,7 @@ def create_links(routers, links):
             neighbor_array[pair[0]].append(pair[1])
             neighbor_array[pair[1]].append(pair[0])
         index_counter += 1
+    print(neighbor_array)
     return neighbor_array
     #for array in neighbor_array:
         #print(array)
@@ -148,7 +152,7 @@ def draw_tables(root, table_values, routers):
     for i in range(1, len(routers)*4 + 1):
         columns.append(i)
     print(columns)
-    
+
     tree = ttk.Treeview(frame, columns = (columns), height = 5, show = "tree")
 
     tree.insert('', 'end', values = router_headers)
